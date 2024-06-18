c208f5070535da31554c315ffae4c6439fd69f71
Hans Nathanael <hansnathanael2004@gmail.com>
2024-06-18 21:38:51 +0700

    Membuat database buktipembayaran untuk admin

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\BarangController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BuktiBayarController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +30,7 @@ Auth::routes();
 
 Route::get('/', [HomeController::class, 'first'])->name('home');
 Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa');
+Route::get('/bukti_bayar', [BuktiBayarController::class, 'index'])->name('bukti_bayar') ;
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
@@ -38,4 +41,9 @@ Route::group(['middleware' => 'auth'], function () {
     Route::delete('/siswa/destroy/{id}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
     Route::get('/payment', [HomeController::class, 'payment'])->name('payment');
     Route::get('/request', [HomeController::class, 'request'])->name('request');
+    Route::get('/bukti_bayar/payment', [BuktiBayarController::class, 'payment'])->name('bukti_bayar.payment');
+    Route::get('/bukti_bayar/confirm/{id}', [BuktiBayarController::class, 'confirm'])->name('bukti_bayar.confirm');
+    Route::post('/bukti_bayar/store', [BuktiBayarController::class, 'store'])->name('bukti_bayar.store');
+    Route::put('/bukti_bayar/update/{id}', [BuktiBayarController::class, 'update'])->name('bukti_bayar.update');
+    Route::delete('/bukti_bayar/destroy/{id}', [BuktiBayarController::class, 'destroy'])->name('bukti_bayar.destroy');
 });
