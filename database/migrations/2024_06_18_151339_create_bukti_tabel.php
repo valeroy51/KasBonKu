4b9064b1996454a693edf08a96be33150f901d9a
Hans Nathanael <hansnathanael2004@gmail.com>
2024-06-25 18:36:07 +0700

    Revisi id pada form pembayaran

--- database/migrations/2024_06_18_151339_create_bukti_tabel.php
+++ database/migrations/2024_06_18_151339_create_bukti_tabel.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('bukti_bayar', function (Blueprint $table){
-            $table->uuid('id')->primary();
+            $table->id();
             $table->String('nama');
             $table->String('kelas');
             $table->String('harga');
